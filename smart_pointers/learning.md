b7ace0dd4ef1b7b0c7527bb03ee4500e83634894
Taimoor khan <khant8k83@gmail.com>
2021-09-16 17:52:09 +0500

    box pointer continue

--- smart_pointers/learning.md
+++ smart_pointers/learning.md
@@ -11,3 +11,7 @@ Boxes don’t have performance overhead, other than storing their data on the he
 - When you have a type whose size can’t be known at compile time and you want to use a value of that type in a context that requires an exact size
 - When you have a large amount of data and you want to transfer ownership but ensure the data won’t becopied when you do so
 - When you want to own a value and you care only that it’s a type that implements a particular traitrather than being of a specific type
+
+## Implicit Deref Coercions with Functions and Methods
+
+Deref coercion is a convenience that Rust performs on arguments to functions and methods. Deref coercion works only on types that implement the Deref trait. Deref coercion converts such a type into a reference to another type. For example, deref coercion can convert &String to &str because String implements the Deref trait such that it returns &str
--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,20 +1,44 @@
-use std::fmt;
+// use std::fmt;
 
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
+// impl fmt::Display for List {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f,"({})",self)
+//     }
+// }
 
+// use  crate::List::{Cons, Nil};
+use std::ops::Deref;
+#[derive(Debug)]
+struct MyBox<T>(T);
 
-impl fmt::Display for List {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f,"({})",self)
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
     }
 }
 
-use  crate::List::{Cons, Nil};
+impl<T> Deref for MyBox<T> {
+    type Target = T; // associated type
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-    println!("{}", list);
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // println!("{}", list);
+    let x = 5;
+    let y = MyBox::new(x);
+    hello(&MyBox::new(String::from("Taimoor")));
+    println!("{}", *y) //  compiler calls *y -> *(y.deref())
+}
+
+fn hello(name: &str) {
+    // this is Deref coercion that converts a type into reference to another type
+    println!("Hello {}", name)
 }
