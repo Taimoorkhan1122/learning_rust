d1a89839e548cba91531037b67f8a6b0a21e832a
Taimoor khan <khant8k83@gmail.com>
2021-10-04 15:53:07 +0500

    implemented multiple emiiter

--- fearless_concurency/src/main.rs
+++ fearless_concurency/src/main.rs
@@ -19,12 +19,34 @@ fn main() {
     // handle.join().unwrap();
 
     let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
+    let tx1 = tx.clone();
+
+    // first transmitter
+    let tx1_handle = thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
             String::from("from"),
             String::from("the"),
+            String::from("first"),
             String::from("thread"),
+        ];
+        for v in vals {
+            tx1.send(v).unwrap_or_else(|e| {
+                println!("error in sending message: {}", e);
+                process::exit(1);
+            });
+            thread::sleep(Duration::from_millis(1000))
+        }
+    });
+
+    // second transmitter
+    let tx_handle = thread::spawn(move || {
+        tx1_handle.join().unwrap();
+        let vals = vec![
+            String::from("got"),
+            String::from("some"),
+            String::from("more"),
+            String::from("from second"),
         ];
         for v in vals {
             tx.send(v).unwrap_or_else(|e| {
@@ -42,8 +64,9 @@ fn main() {
     // println!("Got message: {}", received);
 
     // receiving multiple values
-        let time_recv = time::Instant::now();
+    // tx_handle.join().unwrap();
+    let time_recv = time::Instant::now();
     for rec in rx {
-        println!("Got message: {} at {:?} secs", rec, time_recv.elapsed().as_secs());
+        println!("Got message: {:15} at {} secs",rec,time_recv.elapsed().as_secs());
     }
 }
